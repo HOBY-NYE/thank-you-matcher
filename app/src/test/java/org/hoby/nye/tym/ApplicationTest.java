f8896a1a3368bc319013c50932abe686247cdd98
Tim Walshjamin <walsht@gmail.com>
2021-05-19 12:05:11 -0400

    Application launch

---
+++ app/src/test/java/org/hoby/nye/tym/ApplicationTest.java
@@ -0,0 +1,12 @@
+package org.hoby.nye.tym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+
+    }
+}
